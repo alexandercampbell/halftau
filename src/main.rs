ed9185ad35350690102f396bfbc9260d79dd62da
Alexander Campbell <alexanderhcampbell@gmail.com>
2019-10-19 12:09:18 -0500

    Implement lispier lexer + parser

--- src/main.rs
+++ src/main.rs
@@ -1,187 +1,142 @@
-use std::collections::HashMap;
-
 #[derive(Debug, PartialEq)]
-enum Token {
+enum TokenType {
     ParenL,
     ParenR,
-    Ident(String),
-    Grouper,
-    Newline,
-    Tab,
-}
-use Token::*;
 
-fn tokenize(s: String) -> Vec<Token> {
+    Ident(String),
+}
+use TokenType::*;
+
+#[derive(Debug, PartialEq)]
+struct Token {
+    _type: TokenType,
+    line_number: usize,
+}
+
+fn tokenize(s: String) -> Result<Vec<Token>, String> {
     let mut tokens: Vec<Token> = vec![];
     let mut current_ident = String::new();
-
-    let ident_terminators = vec!['\n', ' ', '(', ')', '$', '\t'];
+    let mut line_number = 1;
+    let mut in_ident = false;
 
     for ch in s.chars() {
-        if ident_terminators.contains(&ch) {
-            // ident terminators
-
-            if !current_ident.is_empty() {
-                tokens.push(Ident(current_ident));
-                current_ident = String::new();
+        if ch.is_alphabetic() && !in_ident {
+            current_ident = String::new();
+            current_ident.push(ch);
+            in_ident = true;
+        } else if in_ident && (ch.is_alphanumeric() || ch == '-' || ch == '_' || ch == '?') {
+            current_ident.push(ch);
+        } else {
+            if in_ident {
+                in_ident = false;
+                tokens.push(Token {
+                    _type: Ident(current_ident.clone()),
+                    line_number: line_number,
+                });
             }
 
             match ch {
-                '\n' => tokens.push(Newline),
-                '\t' => tokens.push(Tab),
-                '$' => tokens.push(Grouper),
-                '(' => tokens.push(ParenL),
-                ')' => tokens.push(ParenR),
-                _ => (),
-            };
-        } else {
-            current_ident.push(ch);
+                '(' => tokens.push(Token {
+                    _type: ParenL,
+                    line_number: line_number,
+                }),
+                ')' => tokens.push(Token {
+                    _type: ParenR,
+                    line_number: line_number,
+                }),
+                '\n' => line_number += 1,
+                ' ' => (),
+                '\t' => (),
+                _ => {
+                    return Err(format!(
+                        "unrecognized character {:?} on line {}",
+                        ch, line_number
+                    ))
+                }
+            }
         }
     }
 
-    tokens
+    Ok(tokens)
 }
 
 #[derive(Debug, PartialEq, Clone)]
 enum Node {
     FunctionCall(Vec<Node>),
-    Variable(String),
+    Reference(String),
 }
 use Node::*;
 
-fn parse_function_call(tokens: &[Token]) -> (Node, &[Token]) {
-    let mut params: Vec<Node> = vec![];
-    let mut tokens = tokens;
+fn parse_function_call(tokens: &[Token], index: usize) -> Result<(Node, usize), String> {
+    let mut elts = vec![];
+    let mut index = index;
+    let start_line_number;
 
-    loop {
-        match tokens.get(0) {
-            Some(Ident(name)) => {
-                params.push(Variable(name.to_string()));
-                tokens = &tokens[1..];
-            }
-            Some(Grouper) => {
-                let (final_param, tail) = parse_function_call(&tokens[1..]);
-                params.push(final_param);
-                tokens = tail;
-            }
-            Some(Newline) => match tokens.get(1) {
-                Some(Tab) => {
-                    tokens = &tokens[2..];
-                }
-                _ => {
-                    tokens = &tokens[1..];
-                    break;
-                }
-            },
-            Some(k) => {
-                panic!("unrecognized token {:?}", k);
-            }
-            None => {
-                break;
-            }
+    println!("parsing function call {}", index);
+
+    match tokens.get(index) {
+        Some(Token {
+            _type: ParenL,
+            line_number: l,
+        }) => {
+            index += 1;
+            start_line_number = l.clone();
         }
-    }
-    (FunctionCall(params), tokens)
-}
 
-fn parse(tokens: &[Token]) -> Vec<Node> {
-    let mut nodes: Vec<Node> = vec![];
-    let mut tokens = tokens;
-
-    loop {
-        let token = tokens.get(0);
-        if token.is_none() {
-            break;
+        Some(Token { line_number, .. }) => {
+            return Err(format!(
+                "expected open paren on line {}, got {:?}",
+                line_number,
+                tokens.get(index)
+            ))
         }
-        let token = token.unwrap();
 
-        match token {
-            Ident(_) => {
-                let (node, remaining) = parse_function_call(&tokens);
-                nodes.push(node);
-                tokens = remaining;
-            }
-
-            Tab => tokens = &tokens[1..],
-            Newline => tokens = &tokens[1..],
-            _ => panic!("unrecognized token {:?}", token),
-        }
-    }
-    nodes
-}
-
-#[derive(Debug)]
-enum Function {
-    Defintion,
-    UserDefined { arguments: Vec<Node>, body: Node },
-}
-
-#[derive(Debug)]
-struct Runtime {
-    definitions: HashMap<String, Function>,
-}
-
-impl Runtime {
-    fn resolve_function(&self, node: &Node) -> Function {
-        match node {
-            Variable(text) => match &text[..] {
-                "fn" => Function::Defintion {},
-                _ => panic!("unrecognized function name"),
-            },
-            _ => panic!("function call attemped with non-name"),
-        }
-    }
-
-    fn execute_function(&mut self, func: &Function, arguments: &[Node]) {
-        println!(
-            "executing function call {:?} with args {:?}",
-            func, arguments
-        );
-
-        match func {
-            Function::Defintion => {
-                let name = match arguments.get(0) {
-                    Some(Variable(text)) => text,
-                    Some(_) => panic!("fn can only accept a variable name as its first parameter"),
-                    None => panic!("insufficient parameters to fn"),
-                };
-                println!("defining new function {:?}", name);
-
-                assert!(arguments.len() >= 2);
-                let function_arguments = &arguments[1..arguments.len() - 1];
-                let body = arguments[1..].get(arguments.len() - 2).unwrap();
-
-                self.definitions.insert(
-                    name.to_string(),
-                    Function::UserDefined {
-                        arguments: function_arguments.to_vec(),
-                        body: body.clone(),
-                    },
-                );
-            }
-
-            Function::UserDefined { .. } => {}
-        }
-    }
-}
-
-fn execute(program: Vec<Node>) {
-    let mut runtime = Runtime {
-        definitions: HashMap::new(),
+        None => return Err(String::from("unexpected end of input")),
     };
 
-    for node in program {
-        match node {
-            FunctionCall(nodes) => {
-                assert!(nodes.len() > 0);
-                let func = runtime.resolve_function(&nodes[0]);
-                runtime.execute_function(&func, &nodes[1..]);
+    loop {
+        let token = tokens.get(index);
+        match token {
+            Some(Token { _type: ParenR, .. }) => {
+                println!("constructed function call with elts {:?}", elts);
+                return Ok((FunctionCall(elts), index + 1));
             }
-            _ => panic!("only function calls are allowed at the top level"),
+            Some(Token {
+                _type: Ident(n), ..
+            }) => {
+                elts.push(Reference(n.clone()));
+                index += 1;
+            }
+            Some(Token { _type: ParenL, .. }) => {
+                let (node, new_index) = parse_function_call(tokens, index)?;
+                elts.push(node);
+                index = new_index;
+            }
+            None => {
+                return Err(format!(
+                    "unterminated function definition starting on line {}",
+                    start_line_number
+                ));
+            }
         }
     }
+}
 
-    println!("created definitions: {:?}", runtime);
+fn parse(tokens: &[Token]) -> Result<Vec<Node>, String> {
+    let mut nodes: Vec<Node> = vec![];
+    let mut index = 0;
+    while index < tokens.len() {
+        match parse_function_call(tokens, index) {
+            Ok((node, new_index)) => {
+                nodes.push(node);
+                index = new_index;
+            }
+            Err(s) => {
+                return Err(s);
+            }
+        }
+    }
+    Ok(nodes)
 }
 
 fn main() {
@@ -197,54 +152,13 @@ fn main() {
         file.read_to_string(&mut contents).unwrap();
 
         let tokens = tokenize(contents);
-        let ast = parse(&tokens[..]);
-        execute(ast);
+        match tokens {
+            Ok(tokens) => {
+                println!("Tokens: {:?}", tokens);
+                let ast = parse(&tokens[..]);
+                println!("AST: {:?}", ast);
+            }
+            Err(s) => println!("Error: {}", s),
+        }
     }
-}
-
-#[test]
-fn test_inc_program() {
-    use Token::*;
-
-    let tokens = tokenize(
-        "
-
-        fn inc x $
-        \t+ x 1
-
-        "
-        .to_string(),
-    );
-    assert!(
-        tokens
-            == vec![
-                Newline,
-                Newline,
-                Ident("fn".to_string()),
-                Ident("inc".to_string()),
-                Ident("x".to_string()),
-                Grouper,
-                Newline,
-                Tab,
-                Ident("+".to_string()),
-                Ident("x".to_string()),
-                Ident("1".to_string()),
-                Newline,
-                Newline,
-            ]
-    );
-
-    let ast = parse(tokens.as_slice());
-    assert!(
-        ast == vec![FunctionCall(vec![
-            Variable("fn".to_string()),
-            Variable("inc".to_string()),
-            Variable("x".to_string()),
-            FunctionCall(vec![
-                Variable("+".to_string()),
-                Variable("x".to_string()),
-                Variable("1".to_string())
-            ]),
-        ])]
-    );
 }
