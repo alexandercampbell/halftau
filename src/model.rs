da9c075fa1c6a16988738e60076a8513744f5c54
Alexander Campbell <alexanderhcampbell@gmail.com>
2019-10-24 19:02:39 -0500

    Model: remove unused Type enum

--- src/model.rs
+++ src/model.rs
@@ -51,34 +51,6 @@ pub enum Elt {
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Type {
-    Int,
-    Double,
-    String_,
-    Symbol,
-    List,
-    Vector,
-    Function,
-    Nil,
-    Macro,
-}
-
-pub fn typeof_(v: &Elt) -> Type {
-    match v {
-        Elt::Int(_) => Type::Int,
-        Elt::Double(_) => Type::Double,
-        Elt::String_(_) => Type::String_,
-        Elt::Symbol(_) => Type::Symbol,
-        Elt::List(_) => Type::List,
-        Elt::Vector(_) => Type::Vector,
-        Elt::Function { .. } => Type::Function,
-        Elt::BuiltinFunction(_) => Type::Function,
-        Elt::Nil => Type::Nil,
-        Elt::Macro { .. } => Type::Macro,
-    }
-}
-
-#[derive(Debug, PartialEq, Clone)]
 pub struct Scope {
     pub bindings: HashMap<String, Elt>,
 }
