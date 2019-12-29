a36766656780a613df36efd3002d624ef9c31280
Alexander Campbell <alexanderhcampbell@gmail.com>
2019-12-29 13:35:12 -0600

    main.c: unnecessary stylistic renames

--- halftau-c/main.c
+++ halftau-c/main.c
@@ -4,19 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 enum elt_type
 {
+	// alloc with calloc gives a null elt
+	ELT_TYPE_NULL = 0,
 	ELT_TYPE_INT,
 	ELT_TYPE_DOUBLE,
 	ELT_TYPE_BOOL,
 	ELT_TYPE_STRING,
 	ELT_TYPE_SYMBOL,
-	ELT_TYPE_LIST,
+	ELT_TYPE_CONS,
 	ELT_TYPE_FUNCTION,
 	ELT_TYPE_BUILTIN,
 	ELT_TYPE_MACRO,
-	ELT_TYPE_NIL,
 };
 
 // Could use a union type to reduce the memory footprint of elt.
@@ -26,33 +28,34 @@ struct elt
 
 	// Used for ints and bools.
 	// Bools are truthy iff prim_value_int != 0.
-	int prim_value_int;
+	int64_t prim_value_int;
 
 	// Used only for doubles.
+	// There's no float64_t in the standard,
+	// so... fugg.
 	double prim_value_double;
 
-	// If non-nil, this is guaranteed to be a list of symbols.
+	// If non-null, this is guaranteed to be a list of symbols.
 	// Used by ELT_TYPE_FUNCTION.
 	struct elt *lexical_binds;
 
-	// Used only for lists.
-	struct elt *head, *tail;
+	// Used only for cons.
+	struct elt *car, *cdr;
 };
 
 struct elt *new_elt()
 {
 	// FIXME: this leaks memory.
 	// In the prototyping stage, this is acceptable.
-	struct elt *e = malloc(sizeof(struct elt));
-	memset(e, 0, sizeof(struct elt));
-	e->type = ELT_TYPE_NIL;
+	struct elt *e = calloc(1, sizeof(struct elt));
+	e->type = ELT_TYPE_NULL;
 	return e;
 }
 
 bool truthy(struct elt *e)
 {
 	assert(e);
-	if (e->type == ELT_TYPE_NIL)
+	if (e->type == ELT_TYPE_NULL)
 		return false;
 	if (e->type == ELT_TYPE_BOOL)
 		return e->prim_value_int == 0;
@@ -64,23 +67,22 @@ struct elt *eval(struct elt *e);
 struct elt *eval_function(struct elt *fn, struct elt *args_raw)
 {
 	assert(fn->type == ELT_TYPE_FUNCTION);
-	assert(args_raw->type == ELT_TYPE_LIST);
+	assert(args_raw->type == ELT_TYPE_CONS);
 
 	struct elt *args = new_elt();
-	args->type = ELT_TYPE_LIST;
-	args->head = args->tail = NULL;
+	args->type = ELT_TYPE_CONS;
 
 	struct elt *p = args;
-	while (args_raw->type != ELT_TYPE_NIL)
+	while (args_raw->type)
 	{
-		p->head = eval(args_raw->head);
+		p->car = eval(args_raw->car);
 
-		p->tail = new_elt();
-		p->tail->type = ELT_TYPE_LIST;
-		p = p->tail;
+		p->cdr = new_elt();
+		p->cdr->type = ELT_TYPE_CONS;
+		p = p->cdr;
 
-		args_raw = args_raw->tail;
-		assert(args_raw->type == ELT_TYPE_LIST || args_raw->type == ELT_TYPE_NIL);
+		args_raw = args_raw->cdr;
+		assert(args_raw->type == ELT_TYPE_CONS || args_raw->type == ELT_TYPE_NULL);
 	}
 }
 
@@ -94,19 +96,19 @@ struct elt *eval(struct elt *e)
 	case ELT_TYPE_DOUBLE:
 	case ELT_TYPE_BOOL:
 	case ELT_TYPE_STRING:
-	case ELT_TYPE_NIL:
+	case ELT_TYPE_NULL:
+		// value literals evaluate to themselves
 		return e;
 
-	case ELT_TYPE_LIST:
+	case ELT_TYPE_CONS:
 	{
-		struct elt *function = eval(e->head);
+		struct elt *function = eval(e->car);
 
 		switch (function->type)
 		{
 		case ELT_TYPE_FUNCTION:
-		{
-			return eval_function(function, e->tail);
-		}
+			return eval_function(function, e->cdr);
+
 		default:
 			assert(false && "first elt of eval'd list is not a callable type");
 		}
